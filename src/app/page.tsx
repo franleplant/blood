56ee9c7f9af16b181a96d416e47f104ef44a343c
Fran Guijarro <franleplant@gmail.com>
2025-07-01 16:34:28 -0300

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,14 @@ export default async function Home() {
         <h1 className="text-4xl font-bold text-center">Blood Markers</h1>
       </div>
 
-      <div className="mt-8 flex w-full max-w-5xl flex-col gap-8 lg:flex-row">
-        <div className="w-full lg:w-1/2">
-          <h2 className="text-2xl font-bold text-center my-4">
-            Fasting Glucose
-          </h2>
-          <GlucoseChart userId={1} />
-        </div>
-        <div className="w-full lg:w-1/2">
-          <h2 className="text-2xl font-bold text-center my-4">HOMA-IR</h2>
-          <HomaIRChart userId={1} />
-        </div>
+      <div className="w-full max-w-5xl mt-8">
+        <h2 className="text-2xl font-bold text-center my-4">Fasting Glucose</h2>
+        <GlucoseChart userId={1} />
+      </div>
+
+      <div className="w-full max-w-5xl mt-8">
+        <h2 className="text-2xl font-bold text-center my-4">HOMA-IR</h2>
+        <HomaIRChart userId={1} />
       </div>
 
       <div className="w-full max-w-5xl mt-8">
